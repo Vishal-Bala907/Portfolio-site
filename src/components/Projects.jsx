7db8ab5995e2c7af7f40517a8d3e00b66384e6b7
Vishal bala <vishal.bala.100@gmail.com>
2024-07-15 16:35:04 +0530

    abb ho jaege shayd

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,9 +3,17 @@ import projectInfoArray from "../../myporjects/info";
 import styels from "../styles/projects.module.css";
 import "aos/dist/aos.css";
 import { Link } from "react-router-dom";
-// import cafe1 from "../../myporjects/ProjectImages/cafe1.jpg";
+
+import cafe1 from "../../myporjects/ProjectImages/cafe1.jpg";
+import cafe2 from "../../myporjects/ProjectImages/cafe2.jpg";
+import clock from "../../myporjects/ProjectImages/clock.jpg";
+import react from "../../myporjects/ProjectImages/react.jpg";
+import res from "../../myporjects/ProjectImages/res.jpg";
+import temple from "../../myporjects/ProjectImages/temple.jpg";
+import whe from "../../myporjects/ProjectImages/whe.jpg";
 
 export default function Projects() {
+  let images = [res, whe, cafe2, temple, clock, cafe1, react];
   function mouseOver(id) {
     let desc = document.getElementById(`desc-${id}`);
 
@@ -31,14 +39,14 @@ export default function Projects() {
         className="container-sm bg-dark p-4 d-flex justify-content-center flex-wrap position-relative"
         style={{ height: "fit-content" }}
       >
-        {projectInfoArray.map((project) => {
+        {projectInfoArray.map((project, index) => {
           flag = !flag;
           return (
             <div
               data-aos={flag ? effect[0] : effect[1]}
               key={project.id}
               className={`p-3 m-3 text-center ${styels.parent}`}
-              style={{ backgroundImage: `url('${project.image}')` }}
+              style={{ backgroundImage: `url('${images[index]}')` }}
               onMouseEnter={() => mouseOver(project.id)}
               onMouseLeave={() => mouseLeave(project.id)}
             >

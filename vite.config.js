2876eb073873d07d30f322f06b6cf0dd778a986b
Vishal bala <vishal.bala.100@gmail.com>
2024-07-15 14:12:56 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/", // Correct base path for GitHub Pages
+  base: "/portfolio/portfolio/", // Correct base path for GitHub Pages
   plugins: [react()],
   build: {
     outDir: "dist", // Ensure this matches your deploy script

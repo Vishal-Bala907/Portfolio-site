37c033dbb1d6c98c19a57f58be6abbb1c9bf4af7
Vishal bala <vishal.bala.100@gmail.com>
2024-07-15 13:50:22 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio-App/", // Correct base path for GitHub Pages
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // Ensure this matches your deploy script
+  },
+});

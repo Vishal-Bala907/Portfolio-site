78ced780935116318a8ccc689036fec5cb55e206
Vishal bala <vishal.bala.100@gmail.com>
2024-07-15 14:30:11 +0530

    try 2

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   base: "/portfolio/portfolio/", // Correct base path for GitHub Pages
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure this matches your deploy script
+    outDir: "build", // Ensure this matches your deploy script
   },
 });

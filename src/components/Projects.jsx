1f676ef7cb01187c7166ad6c3eaf54e2dd7f3cd2
Vishal bala <vishal.bala.100@gmail.com>
2024-07-19 12:20:57 +0530

    bug fixes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -64,14 +64,14 @@ export default function Projects() {
                 <p>
                   <b> {project.desc}</b>
                 </p>
-                <a
-                  href={project.site}
+                <Link
+                  to={project.site}
                   type="button"
                   target="_blank"
                   className="btn btn-primary"
                 >
                   View Details
-                </a>
+                </Link>
                 <a
                   href={project.github}
                   target="_blank"

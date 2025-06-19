4dce2293642df056e70b0470b8738e5b606d9a51
Vishal bala <152321566+Vishal-Bala907@users.noreply.github.com>
2025-06-19 15:05:00 +0530

    Update vite.config.js
    deployement fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-site/", // Correct base path for GitHub Pages
+  base: "/", // ✅ Change this from "/Portfolio-site/" to "/"
   plugins: [react()],
   build: {
-    outDir: "build", // Ensure this matches your deploy script
+    outDir: "build",
   },
 });
